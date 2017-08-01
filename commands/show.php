7bd3bef39e00cedfc69e5acc16205f735a7f555d
Sidsector9 <siddharth.thevaril@gmail.com>
2017-08-01 15:30:01 +0530

    Show Command

---
+++ commands/show.php
@@ -0,0 +1,29 @@
+<?php
+class Show_Command extends WP_CLI_Command {
+	/**
+	 * @when before_wp_load
+	 */
+	public function __invoke( $_, $assoc_args ) {
+		global $db;
+
+		// return is site name is empty.
+		if ( empty( $_[0] ) ) {
+			WP_CLI::error( 'Site name cannot be empty.' );
+			return;
+		} else {
+			$site_name = $_[0];
+		}
+
+		$result = $db->query( 'SELECT site_name FROM ee_site_data WHERE site_name="' . $site_name . '"' );
+
+		if ( empty( $result->fetchArray() ) ) {
+			WP_CLI::error( $site_name . ' does not exist' );
+			die;
+		} else {
+			$result = $db->query( 'DELETE FROM ee_site_data WHERE site_name="' . $site_name . '"' );
+			WP_CLI::success( $site_name . ' successfully deleted.' );
+		}
+	}
+}
+
+WP_CLI::add_command( 'ee site show', 'Show_Command' );

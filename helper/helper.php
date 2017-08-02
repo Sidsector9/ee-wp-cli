f283e39fd8a3039d4213644b24a80b613547d4d6
Sidsector9 <siddharth.thevaril@gmail.com>
2017-08-02 15:29:05 +0530

    Add prepare update query

--- helper/helper.php
+++ helper/helper.php
@@ -27,10 +27,29 @@ function _prepare_insert_query() {
 	return 'INSERT INTO ee_site_data (' . $column_names . ') VALUES (' . $column_values . ')';
 }
 
+function _prepare_update_query() {
+	$count        = 2;
+	$column       = func_get_arg( 0 );
+	$site_name    = func_get_arg( 1 );
+	$column_names = '';
+
+	while ( $count < func_num_args() ) {
+		$column_names .= $column[ $count - 2 ] . "='" . ( null === func_get_arg( $count ) ? 'NULL' : func_get_arg( $count ) ) . "',";
+		$count++;
+	}
+
+	$column_names = rtrim( $column_names, ',' );
+	return "UPDATE ee_site_data SET " . $column_names . " WHERE site_name='" . $site_name . "'";
+}
+
 /**
  * Generates configuration file for a site.
  */
 function _generate_config_file( $path, $file_name, $config_data, $is_update = false ) {
+	if ( $is_update ) {
+		_delete_configuration_file( CONFIG_DIR, $file_name );
+	}
+
 	if ( file_exists( $path . $file_name . CNF_EXT ) ) {
 		WP_CLI::error( 'A config file with the name ' . $file_name . CNF_EXT . ' already exists.' );
 		die();
@@ -70,4 +89,15 @@ function _get_config( $site_name ) {
 		}
 		WP_CLI::log( $config_details );
 	}
+}
+
+/**
+ * Get column values.
+ */
+function _get_column_values( $site_name, $columns ) {
+	global $db;
+
+	$column_string = implode( ',', $columns );
+	$result = $db->query( 'SELECT ' . $column_string . ' FROM ee_site_data WHERE site_name="' . $site_name . '"' );
+	return $result;
 }
